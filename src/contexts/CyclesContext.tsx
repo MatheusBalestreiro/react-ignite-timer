eb63f60690025b2fb19937f1a9f6ecb317429add
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-05-05 17:05:06 -0300

    Salvando estado no storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
-import { ReactNode, createContext, useReducer, useState } from "react";
+import { ReactNode, createContext, useEffect, useReducer, useState } from "react";
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
-import { ActionTypes, addNewCycleAction, interrupeCurrentCycleAction, markCurrentCycleAsFinishiedAction } from "../reducers/cycles/actions";
+import { addNewCycleAction, interrupeCurrentCycleAction, markCurrentCycleAsFinishiedAction } from "../reducers/cycles/actions";
+import { differenceInSeconds } from "date-fns";
 
 interface CreateCycleData{
   task: string
@@ -29,17 +30,38 @@ interface CyclesContextProviderProps {
 
 
 export function CyclesContextProvider({ children, }: CyclesContextProviderProps) {
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-  const [cyclesState, dispatch] = useReducer(cyclesReducer,
-    {
+  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
       cycles: [],
       activeCycleId: null,
-    },
-  )
+    }, () => {
+      const storedStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state-1.0.0');
 
-  const {cycles,activeCycleId} = cyclesState
+      if (storedStateAsJSON){
+        return JSON.parse(storedStateAsJSON)
+      }
+    })
 
-  const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+    const {cycles,activeCycleId} = cyclesState
+    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() =>{
+    if (activeCycle){
+      return differenceInSeconds(
+        new Date(),
+        new Date(activeCycle.startDate),
+      )
+    }
+    return 0
+  }) 
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
+
+ 
+  
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
