bd5b4324e646ddcd9ef95bede509d751a624009f
MatheusBalestreiro <matheusafonso95@gmail.com>
2023-05-05 16:30:47 -0300

    Separando Actions

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useReducer, useState } from "react";
-import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles'
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
+import { ActionTypes, addNewCycleAction, interrupeCurrentCycleAction, markCurrentCycleAsFinishiedAction } from "../reducers/cycles/actions";
 
 interface CreateCycleData{
   task: string
@@ -45,12 +46,7 @@ export function CyclesContextProvider({ children, }: CyclesContextProviderProps)
   }
 
   function markCurrentCycleAsFinished(){
-    dispatch({
-      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload: {
-        activeCycleId,
-      }
-    })
+    dispatch(markCurrentCycleAsFinishiedAction())
   }
 
   function createNewCycle(data : CreateCycleData){
@@ -63,22 +59,12 @@ export function CyclesContextProvider({ children, }: CyclesContextProviderProps)
       startDate: new Date(),
     }
 
-    dispatch({
-      type: ActionTypes.ADD_NEW_CYCLE,
-      payload: {
-        newCycle,
-      }
-    })
+    dispatch(addNewCycleAction(newCycle))
     setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
-    dispatch({
-      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload: {
-        activeCycleId,
-      }
-    })
+    dispatch(interrupeCurrentCycleAction())
   }
 
   return (
